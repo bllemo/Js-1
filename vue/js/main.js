ddab9cd2fc40f59807f5d813074579ed08c5e523
bllemo <podkovskij.maks1@gmail.com>
2026-02-03 09:35:49 +0700

    5

---
+++ vue/js/main.js
@@ -0,0 +1,24 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks.",
+        link: "More products like this",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        OnSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+    }
+})
