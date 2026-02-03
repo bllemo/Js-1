440dabd446ef66fc7c91a2cb92e8fe8a78bcb7ab
bllemo <podkovskij.maks1@gmail.com>
2026-02-03 10:50:22 +0700

    6

--- vue/js/main.js
+++ vue/js/main.js
@@ -12,13 +12,30 @@ let app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: './assets/vmSocks-green-onWhite.jpg',
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        SubFromCart() {
+            this.cart -= 1
+            if (this.cart <= 0) {
+                this.cart = 0
+            }
+        }
     }
 })
