3f8c97551b92ab9c529becce583aade2afbe5b38
bllemo <podkovskij.maks1@gmail.com>
2026-02-06 16:17:00 +0700

    working step

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,3 +1,53 @@
+Vue.component('product-review', {
+    template: `
+    <input v-model="name">
+    <form class="review-form" @submit.prevent="onSubmit">
+     <p>
+       <label for="name">Name:</label>
+       <input id="name" v-model="name" placeholder="name">
+     </p>
+
+     <p>
+       <label for="review">Review:</label>
+       <textarea id="review" v-model="review"></textarea>
+     </p>
+
+     <p>
+       <label for="rating">Rating:</label>
+       <select id="rating" v-model.number="rating">
+         <option>5</option>
+         <option>4</option>
+         <option>3</option>
+         <option>2</option>
+         <option>1</option>
+       </select>
+     </p>
+     <p>
+       <input type="submit" value="Submit"> 
+     </p>
+    </form>
+ `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null
+        }
+    },
+    methods:{
+        onSubmit() {
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating
+            }
+            this.$emit('review-submitted', productReview)
+            this.name = null
+            this.review = null
+            this.rating = null
+        },
+    }
+})
 Vue.component('product-details', {
     props: {
         details:{
@@ -44,9 +94,21 @@ Vue.component('product', {
         <div v-for="size in sizes">
             <p>{{ size }}</p>
         </div>
-    </div>
+<!--    </div>-->
     <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to cart</button>
     <button v-on:click="subFromCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Sub from cart</button>
+        <product-review @review-submitted="addReview"></product-review>
+                     <div>
+    <h2>Reviews</h2>
+    <p v-if="!reviews.length">There are no reviews yet.</p>
+    <ul>
+      <li v-for="review in reviews">
+      <p>{{ review.name }}</p>
+      <p>Rating: {{ review.rating }}</p>
+      <p>{{ review.review }}</p>
+      </li>
+    </ul>
+    </div>
 </div>
 `,
     data() {
@@ -59,6 +121,7 @@ Vue.component('product', {
             altText: "A pair of socks",
             inStock: true,
             OnSale: true,
+            reviews: [],
             details: ['80% cotton', '20% polyester', 'Gender-neutral'],
             variants: [
                 {
@@ -83,14 +146,15 @@ Vue.component('product', {
         },
         subFromCart() {
              this.$emit('sub-from-cart', this.variants[this.selectedVariant].variantId);
-        //     this.cart -= 1
-        //     if (this.cart <= 0) {
-        //         this.cart = 0
          },
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
+
     },
     computed: {
         title() {
@@ -120,13 +184,14 @@ Vue.component('product', {
         data: {
             premium: true,
             cart: [],
+            review: []
         },
         methods: {
             updateCart(id) {
                 this.cart.push(id);
             },
             subCart(id) {
-                this.cart.pop();
+                this.cart.pop(id);
             }
 
         }
